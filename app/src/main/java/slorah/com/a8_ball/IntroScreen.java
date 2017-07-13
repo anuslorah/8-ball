08c830f3d6bcf8dcd8d661a55361436fb0fc10be
Anu Slorah <anuslorah@hotmail.com>
2017-07-12 19:46:33 -0700

    Initial commit

---
+++ app/src/main/java/slorah/com/a8_ball/IntroScreen.java
@@ -0,0 +1,13 @@
+package slorah.com.a8_ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/slorah/com/a8_ball/SpashScreen.java
@@ -0,0 +1,8 @@
+package slorah.com.a8_ball;
+
+/**
+ * Created by anusl on 7/12/2017.
+ */
+
+public class SpashScreen {
+}
