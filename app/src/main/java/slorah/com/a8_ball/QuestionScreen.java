62f33a54bcd054514df906e5f88d89a211056fb2
ZakBrinlee <zbrinlee@hotmail.com>
2017-07-31 19:12:02 -0700

    Added Spinner Adapter V1
    The Spinner adapter populates each spinner with and array. Also assigns the custom layout for the multi-line view

--- app/src/main/java/slorah/com/a8_ball/QuestionScreen.java
+++ app/src/main/java/slorah/com/a8_ball/QuestionScreen.java
@@ -1,15 +1,22 @@
-package slorah.com.a8_ball;
+package com.example.zbrinlee.magic8ball;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Created by anusl on 7/12/2017.
  */
 
 public class QuestionScreen extends AppCompatActivity {
+
+    private Spinner question1Spinner;
+    private Spinner question2Spinner;
+    private Spinner question3Spinner;
+    private Spinner question4Spinner;
 
     private Button submitbutton;
     @Override
@@ -20,6 +27,38 @@ public class QuestionScreen extends AppCompatActivity {
 
         //get references to the widget
         submitbutton = (Button) findViewById(R.id.submitbutton);
+
+        //get reference to spinners
+        question1Spinner = (Spinner) findViewById(R.id.question1Spinner);
+        question2Spinner = (Spinner) findViewById(R.id.question2Spinner);
+        question3Spinner = (Spinner) findViewById(R.id.question3Spinner);
+        question4Spinner = (Spinner) findViewById(R.id.question4Spinner);
+
+        //create array adapter for specified array and layout
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.Questions1,     R.layout.textview);
+        question1Spinner.setAdapter(adapter1);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.Questions2,     R.layout.textview);
+        question2Spinner.setAdapter(adapter2);
+
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.Questions3,     R.layout.textview);
+        question3Spinner.setAdapter(adapter3);
+
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this, R.array.Questions4,     R.layout.textview);
+        question3Spinner.setAdapter(adapter4);
+
+        //set the layout for the drop-down
+        adapter1.setDropDownViewResource(R.layout.textview);
+        question1Spinner.setAdapter(adapter1);
+
+        adapter2.setDropDownViewResource(R.layout.textview);
+        question2Spinner.setAdapter(adapter2);
+
+        adapter3.setDropDownViewResource(R.layout.textview);
+        question3Spinner.setAdapter(adapter3);
+
+        adapter4.setDropDownViewResource(R.layout.textview);
+        question4Spinner.setAdapter(adapter4);
 
     }
     //clicking the button transitions to transitionlayout
